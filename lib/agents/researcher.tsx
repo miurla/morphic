a431b06347fc74144cf1f4477462a4b1d659cba6
Yoshiki Miura <miurap400@gmail.com>
2024-04-08 23:39:46 +0900

    Update researcher instructions to include images

--- lib/agents/researcher.tsx
+++ lib/agents/researcher.tsx
@@ -34,7 +34,7 @@ export async function researcher(
     maxTokens: 2500,
     system: `As a professional search expert, you possess the ability to search for any information on the web. 
     For each user query, utilize the search results to their fullest potential to provide additional information and assistance in your response.
-    If there are any key images relevant to your answer, be sure to include them as well.
+    If there are any images relevant to your answer, be sure to include them as well.
     Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.
     Whenever quoting or referencing information from a specific URL, always cite the source URL explicitly.
     `,
