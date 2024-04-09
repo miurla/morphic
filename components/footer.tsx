a7dd81106ae1f5e4db4fb3b235ed1ffe1c9b0257
Emre Elbeyoglu <87910796+aeelbeyoglu@users.noreply.github.com>
2024-04-09 14:39:07 +0300

    footer fix

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,37 +1,20 @@
 import React from 'react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiTwitter } from 'react-icons/si'
+import { SiGooglechrome } from 'react-icons/si'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0">
       <div className="flex justify-end">
+    
         <Button
           variant={'ghost'}
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://discord.gg/zRxaseCuGq" target="_blank">
-            <SiDiscord size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://twitter.com/miiura" target="_blank">
-            <SiTwitter size={18} />
-          </Link>
-        </Button>
-        <Button
-          variant={'ghost'}
-          size={'icon'}
-          className="text-muted-foreground/50"
-        >
-          <Link href="https://git.new/morphic" target="_blank">
-            <SiGithub size={18} />
+          <Link href="https://seomator.com/ai-seo-assistant" target="_blank">
+            <SiGooglechrome size={18} />
           </Link>
         </Button>
       </div>
