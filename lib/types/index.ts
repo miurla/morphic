e213c0421260bed1760deb33217c6de374347e8a
Yoshiki Miura <miurap400@gmail.com>
2024-04-29 01:02:38 +0900

    Add chat functionality using Redis

--- lib/types/index.ts
+++ lib/types/index.ts
@@ -1,3 +1,5 @@
+import { Message } from 'ai'
+
 export type SearchResults = {
   images: string[]
   results: SearchResultItem[]
@@ -8,4 +10,14 @@ export type SearchResultItem = {
   title: string
   url: string
   content: string
+}
+
+export interface Chat extends Record<string, any> {
+  id: string
+  title: string
+  createdAt: Date
+  userId: string
+  path: string
+  messages: Message[]
+  sharePath?: string
 }
