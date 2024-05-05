8770ab00aa216954e087d703ff1b86cecf359bbf
Chen Liu <chen@txz.tech>
2024-05-05 22:29:13 +0800

    update metadata on search page

--- app/search/[id]/page.tsx
+++ app/search/[id]/page.tsx
@@ -13,8 +13,28 @@ export interface SearchPageProps {
 
 export async function generateMetadata({ params }: SearchPageProps) {
   const chat = await getChat(params.id, 'anonymous')
+  const chatTitle = chat?.title.toString().slice(0, 50)
+  const title = chatTitle || '信源AI助手 - 对动态、复杂、高维数据进行智能分析'
+  const description = chatTitle
+    ? '信源AI助手 - 对动态、复杂、高维数据进行智能分析'
+    : '基于先进的数据+AI一体化引擎，赋能企业对运营生产动态的实时监控与掌握，实现生产经营的降本增益！'
   return {
-    title: chat?.title.toString().slice(0, 50) || 'Search'
+    title,
+    description,
+    openGraph: {
+      title,
+      description,
+      siteName: title,
+      type: 'website',
+      images: [
+        {
+          url: `/opengraph-image.png`, // Must be an absolute URL
+          width: 512,
+          height: 512,
+          alt: '信源AI助手'
+        }
+      ]
+    }
   }
 }
 
