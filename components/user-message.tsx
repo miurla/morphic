b11e180d55abf732d988b5ed494e2fe977d2ad77
Yoshiki Miura <miurap400@gmail.com>
2024-05-11 21:37:00 +0900

    Add min-height to user message container

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -13,7 +13,7 @@ export const UserMessage: React.FC<UserMessageProps> = ({
   showShare = false
 }) => {
   return (
-    <div className="flex items-center w-full space-x-1 mt-2">
+    <div className="flex items-center w-full space-x-1 mt-2 min-h-10">
       <div className="text-xl flex-1">{message}</div>
       {showShare && chatId && <ChatShare chatId={chatId} />}
     </div>
