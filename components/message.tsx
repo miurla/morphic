ac4434505df46e283d5d78a42993e8b44f0967f2
Yoshiki Miura <miurap400@gmail.com>
2024-05-15 17:09:48 +0900

    Modify message component to render LaTeX equations

--- components/message.tsx
+++ components/message.tsx
@@ -14,13 +14,30 @@ export function BotMessage({ content }: { content: StreamableValue<string> }) {
   // Currently, sometimes error occurs after finishing the stream.
   if (error) return <div>Error</div>
 
+  //modify the content to render LaTeX equations
+  const processedData = preprocessLaTeX(data || '')
+
   return (
     <MemoizedReactMarkdown
       rehypePlugins={[[rehypeExternalLinks, { target: '_blank' }], rehypeKatex]}
       remarkPlugins={[remarkGfm, remarkMath]}
       className="prose-sm prose-neutral prose-a:text-accent-foreground/50"
     >
-      {data}
+      {processedData}
     </MemoizedReactMarkdown>
   )
+}
+
+// Preprocess LaTeX equations to be rendered by KaTeX
+// ref: https://github.com/remarkjs/react-markdown/issues/785
+const preprocessLaTeX = (content: string) => {
+  const blockProcessedContent = content.replace(
+    /\\\[([\s\S]*?)\\\]/g,
+    (_, equation) => `$$${equation}$$`
+  )
+  const inlineProcessedContent = blockProcessedContent.replace(
+    /\\\(([\s\S]*?)\\\)/g,
+    (_, equation) => `$${equation}$`
+  )
+  return inlineProcessedContent
 }
