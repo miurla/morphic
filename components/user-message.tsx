bbd19ee8c07b7ebc10aeb57ed1f035f4b83b6cfd
Yoshiki Miura <miurap400@gmail.com>
2024-05-19 16:18:52 +0900

    Update user message component to allow line breaks

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -15,7 +15,7 @@ export const UserMessage: React.FC<UserMessageProps> = ({
   const enableShare = process.env.ENABLE_SHARE === 'true'
   return (
     <div className="flex items-center w-full space-x-1 mt-2 min-h-10">
-      <div className="text-xl flex-1">{message}</div>
+      <div className="text-xl flex-1 break-words">{message}</div>
       {enableShare && showShare && chatId && <ChatShare chatId={chatId} />}
     </div>
   )
