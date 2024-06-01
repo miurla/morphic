e10b91fb9158e60d69713273aec5cce65edc56c6
Yoshiki Miura <miurap400@gmail.com>
2024-06-01 18:33:35 +0900

    feat: Handle query parameter in URL

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
-import { usePathname, useRouter } from 'next/navigation'
+import { usePathname, useRouter, useSearchParams } from 'next/navigation'
 import { ChatPanel } from './chat-panel'
 import { ChatMessages } from './chat-messages'
 import { useUIState, useAIState } from 'ai/rsc'
@@ -13,14 +13,19 @@ type ChatProps = {
 export function Chat({ id }: ChatProps) {
   const router = useRouter()
   const path = usePathname()
+  const searchParams = useSearchParams()
+  const query = searchParams.get('q') || undefined
   const [messages] = useUIState()
   const [aiState] = useAIState()
 
   useEffect(() => {
-    if (!path.includes('search') && messages.length === 1) {
+    if (
+      (!path.includes('search') && messages.length === 1) ||
+      (path.includes('/search') && query && messages.length === 1)
+    ) {
       window.history.replaceState({}, '', `/search/${id}`)
     }
-  }, [id, path, messages])
+  }, [id, path, messages, query, searchParams])
 
   useEffect(() => {
     if (aiState.messages[aiState.messages.length - 1]?.type === 'followup') {
@@ -32,7 +37,7 @@ export function Chat({ id }: ChatProps) {
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
       <ChatMessages messages={messages} />
-      <ChatPanel messages={messages} />
+      <ChatPanel messages={messages} query={query} />
     </div>
   )
 }
