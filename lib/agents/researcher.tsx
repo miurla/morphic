7e53e7f8d36356367b668d94e9f3a44c06ae0bbf
Yoshiki Miura <miurap400@gmail.com>
2024-06-13 16:09:05 +0900

    chore: Update the researcher's prompt

--- lib/agents/researcher.tsx
+++ lib/agents/researcher.tsx
@@ -35,8 +35,9 @@ export async function researcher(
     If there are any images relevant to your answer, be sure to include them as well.
     Aim to directly address the user's question, augmenting your response with insights gleaned from the search results.
     Whenever quoting or referencing information from a specific URL, always explicitly cite the source URL using the [[number]](url) format. Multiple citations can be included as needed, e.g., [[number]](url), [[number]](url).
+    The number must always match the order of the search results.
     The retrieve tool can only be used with URLs provided by the user. URLs from search results cannot be used.
-    If it is a domain or a site name instead of a URL, specify it in the include_domains of the search tool.
+    If it is a domain instead of a URL, specify it in the include_domains of the search tool.
     Please match the language of the response to the user's language. Current date and time: ${currentDate}`,
     messages: processedMessages,
     tools: getTools({
