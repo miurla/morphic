3b732db1ade025009e1917f7e2a953fbd9f38df8
jeremyperwira <jeremyperwira@protonmail.com>
2024-06-22 21:41:17 +0700

    Make delete button optional

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -21,6 +21,7 @@ export const UserMessage: React.FC<UserMessageProps> = ({
     <div className="flex items-center w-full space-x-1 mt-2 min-h-10">
       <div className="text-xl flex-1 break-words w-full">{message}</div>
       {enableShare && showShare && chatId && <ChatShare chatId={chatId} />}
+      {enableDelete && showDelete && chatId && <ClearChat  chatId={chatId} />}
     </div>
   )
 }
