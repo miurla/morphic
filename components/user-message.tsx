eeccab835825448d29a17c8dbbfe9de5ff672f3a
jeremyperwira <jeremyperwira@protonmail.com>
2024-06-22 21:40:57 +0700

    Make the delete features optional

--- components/user-message.tsx
+++ components/user-message.tsx
@@ -15,6 +15,7 @@ export const UserMessage: React.FC<UserMessageProps> = ({
   showShare = false,
   showDelete = false
 }) => {
+  const enableDelete = process.env.ENABLE_DELETE === 'true'
   const enableShare = process.env.ENABLE_SHARE === 'true'
   return (
     <div className="flex items-center w-full space-x-1 mt-2 min-h-10">
