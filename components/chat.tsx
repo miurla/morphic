ed9a1533d656919d8d2deded551cc66fc87e00cb
Yoshiki Miura <miurap400@gmail.com>
2024-06-23 11:51:03 +0900

    fix: Add useSearchParams hook to Chat component

--- components/chat.tsx
+++ components/chat.tsx
@@ -1,7 +1,7 @@
 'use client'
 
 import { useEffect } from 'react'
-import { usePathname } from 'next/navigation'
+import { usePathname, useSearchParams } from 'next/navigation'
 import { ChatPanel } from './chat-panel'
 import { ChatMessages } from './chat-messages'
 import { useUIState } from 'ai/rsc'
@@ -13,16 +13,17 @@ type ChatProps = {
 
 export function Chat({ id, query }: ChatProps) {
   const path = usePathname()
+  const searchParams = useSearchParams()
   const [messages] = useUIState()
 
   useEffect(() => {
     if (
       (!path.includes('search') && messages.length === 1) ||
-      (path.includes('/search?q=') && query && messages.length === 1)
+      (searchParams.get('q') && messages.length === 1)
     ) {
       window.history.replaceState({}, '', `/search/${id}`)
     }
-  }, [id, path, messages, query])
+  }, [id, path, messages, query, searchParams])
 
   return (
     <div className="px-8 sm:px-12 pt-12 md:pt-14 pb-14 md:pb-24 max-w-3xl mx-auto flex flex-col space-y-3 md:space-y-4">
