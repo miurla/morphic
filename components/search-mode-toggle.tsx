80a0d169fcf22e84c7c405bd02091f64f7630378
UltraV <1216564567@qq.com>
2025-03-14 01:07:40 +0800

    改变searchmode

--- components/search-mode-toggle.tsx
+++ components/search-mode-toggle.tsx
@@ -1,24 +1,15 @@
 'use client'
 
 import { cn } from '@/lib/utils'
-import { getCookie, setCookie } from '@/lib/utils/cookies'
 import { Globe } from 'lucide-react'
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { Toggle } from './ui/toggle'
 
 export function SearchModeToggle() {
   const [isSearchMode, setIsSearchMode] = useState(true)
 
-  useEffect(() => {
-    const savedMode = getCookie('search-mode')
-    if (savedMode !== null) {
-      setIsSearchMode(savedMode === 'true')
-    }
-  }, [])
-
   const handleSearchModeChange = (pressed: boolean) => {
     setIsSearchMode(pressed)
-    setCookie('search-mode', pressed.toString())
   }
 
   return (
