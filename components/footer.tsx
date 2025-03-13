6372d89ec692c1204f24f67206a26560e6e2edbd
UltraV <1216564567@qq.com>
2025-03-13 15:49:15 +0800

    refactor footer component and disable GPT-4o model

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,13 +1,13 @@
 import Link from 'next/link'
 import React from 'react'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
+import { SiGithub } from 'react-icons/si'
 import { Button } from './ui/button'
 
 const Footer: React.FC = () => {
   return (
     <footer className="w-fit p-1 md:p-2 fixed bottom-0 right-0 hidden lg:block">
       <div className="flex justify-end">
-        <Button
+        {/* <Button
           variant={'ghost'}
           size={'icon'}
           className="text-muted-foreground/50"
@@ -24,13 +24,13 @@ const Footer: React.FC = () => {
           <Link href="https://x.com/morphic_ai" target="_blank">
             <SiX size={18} />
           </Link>
-        </Button>
+        </Button> */}
         <Button
           variant={'ghost'}
           size={'icon'}
           className="text-muted-foreground/50"
         >
-          <Link href="https://git.new/morphic" target="_blank">
+          <Link href="https://github.com/aircrushin" target="_blank">
             <SiGithub size={18} />
           </Link>
         </Button>
