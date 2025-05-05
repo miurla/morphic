fd759ed727269cfd7979f3c42114b5316f2afb9b
Yoshiki Miura <miurap400@gmail.com>
2025-05-05 16:53:34 +0900

    refactor: remove deprecated history and sidebar components

--- components/sidebar.tsx
+++
@@ -1,9 +0,0 @@
-import HistoryContainer from './history-container'
-
-export async function Sidebar() {
-  return (
-    <div className="h-screen p-2 fixed top-0 right-0 flex-col justify-center pb-24 hidden lg:flex">
-      <HistoryContainer />
-    </div>
-  )
-}
