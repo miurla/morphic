eaee44bde10cdc7d3e5bbebc5d0393877e0b53d1
Umair Iqbal <98746015+UmairIqbal92@users.noreply.github.com>
2025-05-21 09:49:04 +0300

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -16,9 +16,9 @@ const fontSans = FontSans({
   variable: '--font-sans'
 })
 
-const title = 'Morphic'
+const title = 'LUNCwolf AI'
 const description =
-  'A fully open-source AI-powered answer engine with a generative UI.'
+  'AI-driven insights and alerts for the LUNC ecosystem.'
 
 export const metadata: Metadata = {
   metadataBase: new URL('https://morphic.sh'),
