f96535633d13c3ada08f223de6a0a66a0878d530
jbear <jeremy@henjii.com>
2025-07-12 08:41:44 -0500

    Refactor external link icons in the dashboard component
    Replaced existing icons with new ones from the lucide-react library for improved visual consistency. Updated the 'View Dashboard', 'Report a Problem', and 'View Insights' links accordingly.

--- components/external-link-items.tsx
+++ components/external-link-items.tsx
@@ -1,24 +1,24 @@
 'use client'
 
 import { DropdownMenuItem } from '@/components/ui/dropdown-menu'
+import { AlertCircle, BarChart, Home } from 'lucide-react'
 import Link from 'next/link'
-import { SiDiscord, SiGithub, SiX } from 'react-icons/si'
 
 const externalLinks = [
   {
     name: 'View Dashboard',
     href: 'https://bulldozer.datapilotplus.com',
-    icon: <SiX className="mr-2 h-4 w-4" />
+    icon: <Home className="mr-2 h-4 w-4" />
   },
   {
     name: 'Report a Problem',
     href: 'mailto:info@breakthroughgroup.com',
-    icon: <SiDiscord className="mr-2 h-4 w-4" />
+    icon: <AlertCircle className="mr-2 h-4 w-4" />
   },
   {
     name: 'View Insights',
     href: 'https://bulldozer.datapilotplus.com/feed',
-    icon: <SiGithub className="mr-2 h-4 w-4" />
+    icon: <BarChart className="mr-2 h-4 w-4" />
   }
 ]
 
