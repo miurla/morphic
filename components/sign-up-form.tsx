b98f4825f446a010a1a3b03ebc4d8c115183988e
vishnu v <vishnu.vijayan96@gmail.com>
2025-08-27 08:49:55 -0400

    style(auth): reorder imports in sign-up form

--- components/sign-up-form.tsx
+++ components/sign-up-form.tsx
@@ -1,8 +1,7 @@
 'use client'
-
+import { useState } from 'react'
 import Link from 'next/link'
 import { useRouter } from 'next/navigation'
-import { useState } from 'react'
 
 import { createClient } from '@/lib/supabase/client'
 import { cn } from '@/lib/utils/index'
