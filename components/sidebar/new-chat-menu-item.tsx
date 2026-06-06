9fcb49b4ff92d77945d10e1dc9575bc36db536bb
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-06-05 20:33:16 -0400

    refactor: use native icon for new chat menu item

--- components/sidebar/new-chat-menu-item.tsx
+++ components/sidebar/new-chat-menu-item.tsx
@@ -2,16 +2,16 @@
 
 import Link from 'next/link'
 
-import { IconPlus as Plus } from '@tabler/icons-react'
-
 import { SidebarMenuButton, SidebarMenuItem } from '@/components/ui/sidebar'
+
+import { NativeIcon } from '@/components/native/native-icon'
 
 export function NewChatMenuItem() {
   return (
     <SidebarMenuItem>
       <SidebarMenuButton asChild>
         <Link href="/" className="flex items-center gap-2">
-          <Plus className="size-4" />
+          <NativeIcon name="newChat" className="size-4" />
           <span>New</span>
         </Link>
       </SidebarMenuButton>
