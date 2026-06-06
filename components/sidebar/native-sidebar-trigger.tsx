ce7b4407ebb539a3108ec4bf2a24f6b93cd9dfca
Damon O <141657791+outlaw-dame@users.noreply.github.com>
2026-06-05 21:10:45 -0400

    feat: add native sidebar trigger

---
+++ components/sidebar/native-sidebar-trigger.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import * as React from 'react'
+
+import { cn } from '@/lib/utils'
+
+import { NativeIcon } from '@/components/native/native-icon'
+import { Button } from '@/components/ui/button'
+import { useSidebar } from '@/components/ui/sidebar'
+
+const NativeSidebarTrigger = React.forwardRef<
+  React.ElementRef<typeof Button>,
+  React.ComponentProps<typeof Button>
+>(({ className, onClick, ...props }, ref) => {
+  const { isMobile, open, openMobile, toggleSidebar } = useSidebar()
+  const isSidebarOpen = isMobile ? openMobile : open
+
+  return (
+    <Button
+      ref={ref}
+      data-sidebar="trigger"
+      variant="ghost"
+      size="icon"
+      className={cn('size-6', className)}
+      onClick={event => {
+        onClick?.(event)
+        toggleSidebar()
+      }}
+      {...props}
+    >
+      <NativeIcon
+        name={isSidebarOpen ? 'sidebarOpen' : 'sidebarClosed'}
+        size={18}
+      />
+      <span className="sr-only">Toggle Sidebar</span>
+    </Button>
+  )
+})
+NativeSidebarTrigger.displayName = 'NativeSidebarTrigger'
+
+export { NativeSidebarTrigger }
