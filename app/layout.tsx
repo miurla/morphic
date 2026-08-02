b4c9a87d11a0f9011f55cd39dd7a89b8e702f630
Siddhant Ray <siddhant.ray1589@gmail.com>
2026-08-02 17:05:25 +0530

    Update metadataBase URL to include HTTPS

--- app/layout.tsx
+++ app/layout.tsx
@@ -32,7 +32,7 @@ const description =
   'A fully open-source AI-powered answer engine with a generative UI.'
 
 export const metadata: Metadata = {
-  metadataBase: new URL('ai-morphic.vercel.app'),
+  metadataBase: new URL('https://ai-morphic.vercel.app'),
   title,
   description,
   openGraph: {
